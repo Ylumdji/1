34ca81ee3da99e59aa10feadeae3a12725c529de
Ylumdji <111727141+Ylumdji@users.noreply.github.com>
2022-08-24 08:22:12 +0300

    kvadratka 1.1

--- kvadrat1.cpp
+++ kvadrat1.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-int solve(double  a,double b ,double c,double * x1,double *x2);
-int solve(double  a,double b ,double c,double * x1,double *x2){
-      if (a==(double)0){
-           if (b==(double)0){
-               if (c==(double)0){
+int solve(float  a,float b ,float c,float * x1,float *x2);
+
+
+
+int solve(float  a,float b ,float c,float * x1,float *x2){
+      if (a<pow(10,-7)){
+           if (b<pow(10,-7)){
+               if (c<pow(10,-7)){
                    return -1;
                }else{
                    return 0 ;
@@ -15,13 +18,13 @@ int solve(double  a,double b ,double c,double * x1,double *x2){
               return 1;
            }
       }else{
-            if (b==(double)0){
+            if (b<pow(10,-7)){
                     if (c < 0){
-                       *x1=sqrt((-c)/a);
-                       *x2=-sqrt((-c)/a);
+                       *x1=(float)sqrt((-c)/a);
+                       *x2=(float)-sqrt((-c)/a);
                        return 2;
                     }
-                    if (c==(double)0){
+                    if (c<pow(10,-7)){
                         *x1=0;
                         return 1;
                     }
@@ -29,36 +32,48 @@ int solve(double  a,double b ,double c,double * x1,double *x2){
                         return 0;
                     }
             }else{
+
                 double discriminant=0;
+
+
                 discriminant = b*b-4*a*c;
+
+
                 if (discriminant > 0){
-                       *x1 = (-b+sqrt(discriminant))/(2*a);
-                       *x2 = (-b-sqrt(discriminant))/(2*a);
+                       *x1 = (-b+(float)sqrt(discriminant))/(2*a);
+                       *x2 = (-b-(float)sqrt(discriminant))/(2*a);
                        return 2;
                 }
-                if (discriminant==(double)0){
+
+                if (discriminant<pow(10,-7)){
                        *x1 = (-b)/(2*a);
                        return 1;
                 }
+
                 if (discriminant < 0){
                        return 0;
                 }
             }
 
       }
+return 0;
 }
+
+
+
+
 int main(){
 
 
-    printf("           SOLUTION OF QUADRATIC EQUATION: a(x^2)+b(x)+c=0\n");
+    printf("           |||SOLUTION OF QUADRATIC EQUATION: a(x^2)+b(x)+c=0|||\n");
     printf("ENTER COEFFICIENT VALUES a , b , c\n");
 
 
-    double a=0.0, b=0.0 , c = 0.0 , x1=0.0, x2=0.0 ;
+    float a=0.0, b=0.0 , c = 0.0 , x1=0.0, x2=0.0 ;
 
 
 
-    if(scanf("%lf %lf %lf",&a,&b,&c)!=3){
+    if(scanf("%f %f %f",&a,&b,&c)!=3){
             printf("!!!WRONG FORMAT!!!\nPLEASE,TRY AGAIN");
             return 0;
     }
